f83528bb7bcd1450952756a5c17ca2f97953c1ad
Matej Valocký <matej.valocky@gmail.com>
2024-01-25 22:43:12 +0100

    2. React 3 - React router a vícestránkový web - více obsahu do stránky

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ const App = () => {
       <Route path="/" element={<h1>Home</h1>} />
       <Route path="/movies" element={<h1>Movies</h1>} />
       <Route path="/serials" element={<h1>Serials</h1>} />
-      <Route path="/new" element={<h1>New</h1>} />
+
+      <Route path="/new" element={<div>
+        <h1>Sledujte nové</h1>
+        <h2>Seriály</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia maxime vero numquam minus natus deserunt omnis nemo totam eos at illum quam perferendis earum, error distinctio autem eveniet provident itaque!</p>
+        <h2>Filmy</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, esse?</p>
+      </div>} />
+      
     </Routes>
   </BrowserRouter>
 }
