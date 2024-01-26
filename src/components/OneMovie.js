77eaa16767bf9327f2d420a7e01f6d234cdfcadb
Matej Valocký <matej.valocky@gmail.com>
2024-01-26 22:55:05 +0100

    16. React 3 - React router a vícestránkový web - generování stránek pro filmy

--- src/components/OneMovie.js
+++ src/components/OneMovie.js
@@ -1,13 +1,25 @@
 import { useParams } from "react-router-dom"
+import { Link } from "react-router-dom"
+import movies from "../data"
 
 const OneMovie = () => {
     console.log(useParams())
 
     const { movieId } = useParams()
 
+    const oneSpecificMovie = movies.find((oneMovie) => {
+        return oneMovie.id === parseInt(movieId)
+    })
+
+    const { image, title, description, tags } = oneSpecificMovie
+
 
     return <section>
-        <h2>{movieId}</h2>
+        <h2>{title}</h2>
+        <img src={image} alt="" />
+        <p>{description}</p>
+        <p>{tags}</p>
+        <Link to="/movies" >Späť na všetky filmy</Link>
     </section>
 }
 
--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,6 +1,18 @@
+import movies from "../data"
+import { Link } from "react-router-dom"
+
 const Movies = () => {
     return <section>
         <h1>Filmy</h1>
+        <div>
+            {movies.map( (oneMovie) => {
+                return <article key={oneMovie.id}>
+                    <h2>{oneMovie.title}</h2>
+                    <img src={oneMovie.image} alt="" /> <br />
+                    <Link to={`/all-movies/${oneMovie.id}`}>Viac info</Link>
+                </article>
+            } )}
+        </div>
     </section>
 }
 
